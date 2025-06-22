7183864b6a54d61220c1f6542af39647ef9226cf
ajs <140010746+aboodJS@users.noreply.github.com>
2025-06-22 14:01:27 +0300

    added a Partner Rewards sections

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,12 @@ import GetStarted from './components/getStarted.vue'
 import NavBar from './components/NavBar.vue'
 import RewardShocase from './components/rewardShocase.vue'
 import SideBar from './components/SideBar.vue'
+import PartnerBox from './components/PartnerBox.vue'
 const visible = ref(false)
 </script>
 
 <template>
-  <main :class="[visible ? 'fixed -top-0 overflow-hidden' : 'overflow-y-scroll']">
+  <main :class="[visible ? 'fixed -top-0 overflow-hidden' : '']">
     <NavBar>
       <img
         class="md:hidden"
@@ -108,7 +109,7 @@ const visible = ref(false)
         >
       </main>
     </section>
-    <section class="bg-[#f2f0eb] grid content-evenly gap-5">
+    <section class="bg-[#f2f0eb] grid content-evenly gap-5 pb-10">
       <div class="grid justify-center text-center mb-12">
         <h1 class="md:text-2xl font-bold text-[#1f1f1e]">Cash or card, you earn Stars</h1>
         <p class="w-3/4 text-center justify-self-center">
@@ -187,5 +188,48 @@ const visible = ref(false)
         </div>
       </div>
     </section>
+    <section id="partner-rewards" class="bg-[#0f4c3a] h-[90vh] grid justify-center content-center">
+      <div class="w-[95vw] h-[80vh] bg-[#d4e9e2] grid">
+        <div class="text-center h-28 grid justify-center content-center gap-6">
+          <h1 class="text-2xl font-bold">Keep the Rewards Coming</h1>
+          <p class="font-semibold max-md:text-sm">
+            The Rewards don't stop at your morning coffee. Join Starbucks® Rewards and unlock perks
+            from our partners, all while earning more Stars.
+          </p>
+        </div>
+        <div class="md:flex max-md:grid max-md:gap-3 justify-evenly">
+          <PartnerBox
+            img="https://www.starbucks.com/weblx/images/rewards/loyalty-partnerships/delta-skymiles.png"
+            name="Delta SkyMiles"
+            >and Starbucks® Rewards accounts to earn miles on Starbucks Card reloads of $25 or
+            more, plus Double Stars on travel days.1</PartnerBox
+          >
+          <PartnerBox
+            img="https://www.starbucks.com/weblx/images/rewards/loyalty-partnerships/bank-of-america.png"
+            name="Bank Of America"
+            >eligible card and Starbucks® Rewards account to earn 2% Cash Back and Bonus Stars on
+            qualifying Starbucks in-app purchases.2</PartnerBox
+          >
+          <PartnerBox
+            img="https://www.starbucks.com/weblx/images/rewards/loyalty-partnerships/marriott-bonvoy/marriott-logo.png"
+            name="Marriot Bonvoy"
+          >
+            and Starbucks® Rewards accounts to earn Double Stars during eligible stays, points
+            during any Marriott Bonvoy Week and more.3
+          </PartnerBox>
+        </div>
+        <button
+          class="font-bold bg-[#046241] p-2 rounded-3xl text-white justify-self-center h-fit mt-10"
+        >
+          Join Starbucks® Rewards
+        </button>
+      </div>
+    </section>
   </main>
 </template>
+
+<style>
+#partner-rewards {
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='18' viewBox='0 0 100 18'%3E%3Cpath fill='%23046241' fill-opacity='1' d='M61.82 18c3.47-1.45 6.86-3.78 11.3-7.34C78 6.76 80.34 5.1 83.87 3.42 88.56 1.16 93.75 0 100 0v6.16C98.76 6.05 97.43 6 96 6c-9.59 0-14.23 2.23-23.13 9.34-1.28 1.03-2.39 1.9-3.4 2.66h-7.65zm-23.64 0H22.52c-1-.76-2.1-1.63-3.4-2.66C11.57 9.3 7.08 6.78 0 6.16V0c6.25 0 11.44 1.16 16.14 3.42 3.53 1.7 5.87 3.35 10.73 7.24 4.45 3.56 7.84 5.9 11.31 7.34zM61.82 0h7.66a39.57 39.57 0 0 1-7.34 4.58C57.44 6.84 52.25 8 46 8S34.56 6.84 29.86 4.58A39.57 39.57 0 0 1 22.52 0h15.66C41.65 1.44 45.21 2 50 2c4.8 0 8.35-.56 11.82-2z'%3E%3C/path%3E%3C/svg%3E");
+}
+</style>
---
+++ src/components/PartnerBox.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps(['img', 'name'])
+</script>
+
+<template>
+  <main class="grid justify-center text-center gap-6">
+    <img :src="img" alt="" class="h-[85px] w-[190px] justify-self-center" />
+    <p class="w-3/4 justify-self-center">
+      <a href="#" class="text-[#00754a] underline">link your {{ name }}</a
+      ><slot></slot>
+    </p>
+  </main>
+</template>
