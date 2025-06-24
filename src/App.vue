f79fc6d9f884c449ec742d300a572696ba90f7d0
ajs <140010746+aboodJS@users.noreply.github.com>
2025-06-25 00:24:50 +0300

    added a restrictions section

--- src/App.vue
+++ src/App.vue
@@ -237,6 +237,91 @@ const visible = ref(false)
         </p>
       </span>
     </div>
+    <section class="h-[110vh] bg-[#F9F9F9] w-screen">
+      <p class="text-xs grid gap-5 w-screen mb-12">
+        <i>At participating stores. Restrictions apply. </i
+        ><i>
+          1Excludes taxes and gratuities. At participating stores. Some restrictions apply. Flights
+          purchased close to departure may not earn double Stars. Stars and miles may not be earned
+          on purchases of alcohol, Starbucks Cards and Starbucks Card reloads. For details, visit
+          <a href="#" class="text-[#04754A] underline">deltastarbucks.com/terms</a>
+        </i>
+        <i
+          >2At participating stores only. Some restrictions apply. Linked Card members will earn 2%
+          Cash Back on the full purchase price of every Qualifying Purchase. Extra Star offer
+          excludes taxes and tips. Stars may not be earned on purchases of alcohol or on reloads of
+          Starbucks Cards that are not registered. For details, visit
+          <a href="#" class="text-[#04754A] underline">Terms and Conditions</a> . Bank of America,
+          N.A. Member FDIC</i
+        ><i
+          >3Individuals must link loyalty program accounts to participate in offer. Members with
+          linked accounts will earn: (a) double Starbucks Rewards Stars on Qualifying Starbucks
+          Transactions during Eligible Stays at Participating Marriott Bonvoy Hotels, excluding
+          stays at Homes & Villas by Marriott Bonvoy and The Ritz-Carlton Yacht Collection, and (b)
+          earn 100 Marriott Bonvoy Points upon completion of 3 Qualifying Starbucks Transactions
+          during Marriott Bonvoy Week. Qualifying Starbucks Transactions must be made at U.S.
+          participating Starbucks stores and exclude purchases of alcoholic beverages, Starbucks
+          Card eGifts and physical Starbucks gift. To earn Double Stars, the applicable stay must
+          qualify as an Eligible Stay at a Participating Marriott Bonvoy® Hotel under offer terms
+          and conditions. Other restrictions and exclusions apply. For full offer terms and
+          conditions, visit
+          <a href="#" class="text-[#04754A] underline"> Starbucks.com/MarriottBonvoy </a>
+          . Marriott Bonvoy® and its affiliated brands and hotels are the registered trademarks
+          and/or service marks of Marriott International, Inc. and its affiliates. Used under
+          license from Marriott International, Inc.↩
+        </i>
+      </p>
+      <main class="grid md:grid-cols-2 md:grid-rows-2 gap-x-5 max-md:gap-5 max-md:text-sm">
+        <div class="grid gap-5 content-center">
+          <h1 class="text-[#686868] font-bold">Earning Stars</h1>
+          <p>
+            Stars cannot be earned on purchases of alcohol, Starbucks Cards or Starbucks Card
+            reloads.
+          </p>
+          <p>
+            Earn 1 Star per $1 spent when you scan your member barcode in the app, then pay with
+            cash, credit/debit cards or mobile wallets at participating stores. You can also earn 1
+            Star per $1 spent when you link a payment method and pay directly through the app.
+          </p>
+          <p>
+            Earn 2 Stars per $1 spent when you load funds and pay with your digital Starbucks Card
+            in the app. You can also earn 2 Stars per $1 spent when you pay in-person at a
+            participating store with your registered physical Starbucks Card or scan your member
+            barcode in the app, and then use any physical Starbucks Card (regardless of whether it
+            is registered) to complete the purchase.
+          </p>
+        </div>
+        <div class="grid gap-5 content-center">
+          <h1 class="text-[#686868] font-bold">Terms of Use</h1>
+          <p>
+            For full program details visit
+            <a href="#" class="text-[#04754A] underline"> starbucks.com/rewards/terms </a>
+          </p>
+          <p>
+            Starbucks® Rewards benefits are available at participating Starbucks stores. Not all
+            stores have the ability to honor Rewards at this time. Visit the
+            <a href="#" class="text-[#04754A] underline"> Starbucks Store Locator</a> and search for
+            locations honoring “Redeem Rewards”.
+          </p>
+        </div>
+        <div class="grid gap-5 content-center">
+          <h1 class="text-[#686868] font-bold">Benefits</h1>
+          <p>
+            Free refills available during same in-store visit only. To qualify for the Birthday
+            Reward, you must have made at least one Star-earning transaction.
+          </p>
+        </div>
+        <div class="grid gap-5 content-center">
+          <h1 class="text-[#686868] font-bold">Redeeming Rewards</h1>
+          <p>
+            Rewards cannot be redeemed for alcoholic beverages or multi-serve items. You pay the
+            difference for any beverage customization over $1 and/or merchandise item over $20. Not
+            all stores honor tiered Rewards. Select stores redeem 200 Stars for free food or drink
+            items only.
+          </p>
+        </div>
+      </main>
+    </section>
   </main>
 </template>
 
