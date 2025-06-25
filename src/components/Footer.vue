616bc1929b7b8ef51575c18a7d4ec7fa87d0a9a0
ajs <140010746+aboodJS@users.noreply.github.com>
2025-06-25 23:19:15 +0300

    made the footer into a component

---
+++ src/components/Footer.vue
@@ -0,0 +1,97 @@
+<script setup></script>
+
+<template>
+  <footer>
+    <section class="h-[70vh] md:flex max-md:grid justify-evenly max-md:content-start max-md:gap-7">
+      <ul class="grid content-evenly max-md:h-7 overflow-hidden">
+        <h1 class="font-semibold">About Us</h1>
+        <i><a class="text-[#B9B9B9] hover:text-black transition-all" href="">Our Company</a></i
+        ><i><a class="text-[#B9B9B9] hover:text-black transition-all" href="">Our Coffee</a></i
+        ><i><a class="text-[#B9B9B9] hover:text-black transition-all" href="">About Starbucks</a></i
+        ><i
+          ><a class="text-[#B9B9B9] hover:text-black transition-all" href=""
+            >StarBucks Archive</a
+          ></i
+        ><i
+          ><a class="text-[#B9B9B9] hover:text-black transition-all" href=""
+            >Investor Relations</a
+          ></i
+        >
+        <i
+          ><a class="text-[#B9B9B9] hover:text-black transition-all" href="">Customer Service</a></i
+        >
+        <i><a class="text-[#B9B9B9] hover:text-black transition-all" href="">Contact Us</a></i>
+      </ul>
+      <ul class="grid content-evenly max-md:h-7 overflow-hidden">
+        <h1 class="font-semibold">Careers</h1>
+        <i
+          ><a class="text-[#B9B9B9] hover:text-black transition-all" href=""
+            >Culture and Values</a
+          ></i
+        ><i
+          ><a class="text-[#B9B9B9] hover:text-black transition-all" href=""
+            >Belonging at Starbucks</a
+          ></i
+        ><i
+          ><a class="text-[#B9B9B9] hover:text-black transition-all" href=""
+            >College Achievement Plan</a
+          ></i
+        ><i
+          ><a class="text-[#B9B9B9] hover:text-black transition-all" href="">Alumni Community</a></i
+        ><i><a class="text-[#B9B9B9] hover:text-black transition-all" href="">US Careers</a></i>
+        <i>
+          <a class="text-[#B9B9B9] hover:text-black transition-all" href=""
+            >International Careers</a
+          >
+        </i>
+      </ul>
+      <ul class="grid content-evenly max-md:h-7 overflow-hidden">
+        <h1 class="font-semibold">Social Impact</h1>
+        <i><a class="text-[#B9B9B9] hover:text-black transition-all" href="">Communities</a></i
+        ><i
+          ><a class="text-[#B9B9B9] hover:text-black transition-all" href=""
+            >StarBucks Foundation</a
+          ></i
+        ><i><a class="text-[#B9B9B9] hover:text-black transition-all" href="">Sustainability</a></i
+        ><i
+          ><a class="text-[#B9B9B9] hover:text-black transition-all" href=""
+            >Enviromental and Social Impact Reporting</a
+          ></i
+        >
+      </ul>
+      <ul class="grid content-evenly max-md:h-7 overflow-hidden">
+        <h1 class="font-semibold">For Business Partners</h1>
+        <i
+          ><a class="text-[#B9B9B9] hover:text-black transition-all" href=""
+            >Landlord Support Center</a
+          ></i
+        ><i><a class="text-[#B9B9B9] hover:text-black transition-all" href="">Suppliers</a></i
+        ><i
+          ><a class="text-[#B9B9B9] hover:text-black transition-all" href=""
+            >Corporate Gift Card Sales</a
+          ></i
+        ><i
+          ><a class="text-[#B9B9B9] hover:text-black transition-all" href=""
+            >Office and FoodService Coffee</a
+          ></i
+        >
+      </ul>
+      <ul class="grid content-evenly max-md:h-7 overflow-hidden">
+        <h1 class="font-semibold">Order and Pick Up</h1>
+        <i><a class="text-[#B9B9B9] hover:text-black transition-all" href="">Order on the App</a></i
+        ><i
+          ><a class="text-[#B9B9B9] hover:text-black transition-all" href="">Order on the Web</a></i
+        ><i><a class="text-[#B9B9B9] hover:text-black transition-all" href="">Delivery</a></i
+        ><i
+          ><a class="text-[#B9B9B9] hover:text-black transition-all" href=""
+            >Order and Pick Up Options</a
+          ></i
+        ><i
+          ><a class="text-[#B9B9B9] hover:text-black transition-all" href=""
+            >Explore and Find Coffee for Home</a
+          ></i
+        >
+      </ul>
+    </section>
+  </footer>
+</template>
