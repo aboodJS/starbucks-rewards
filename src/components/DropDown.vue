2bbf1760cc21e209fcd75cdef2e4f6d07c7eddb1
ajs <140010746+aboodJS@users.noreply.github.com>
2025-06-26 23:39:47 +0300

    made the first part of the footer responsive

---
+++ src/components/DropDown.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+defineProps(['title'])
+const view = ref(false)
+</script>
+
+<template>
+  <ul
+    :class="[
+      'grid content-evenly gap-5 transition-all overflow-hidden',
+      view ? 'max-md:h-fit' : 'max-md:h-7',
+    ]"
+  >
+    <h1 class="font-semibold flex items-center" @click="() => (view = !view)">
+      {{ title }}
+      <div class="md:hidden">
+        <i
+          :class="['bx bx-chevron-down transition-transform text-2xl', view ? 'rotate-180' : '']"
+        ></i>
+      </div>
+    </h1>
+    <slot></slot>
+  </ul>
+</template>
